bc2af8ff43e47bca70dfdeecd823ca881045b4f3
Sebastien Foutrier <sebastien.foutrier@gmail.com>
2012-04-21 11:33:09 +0200

    First commit : Primes util

---
+++ FsUtils/Primes.fs
@@ -0,0 +1,18 @@
+﻿module FsUtils.Primes
+  
+let primesTo maxOfSieve = 
+
+    let rec sieve position elements =
+        if position = Array.length elements then
+            elements
+        else
+            match Array.get elements position with
+            |1 -> sieve (position + 1) elements
+            |n -> 
+                Seq.iter 
+                    (fun i -> Array.set elements (i - 1) 1) 
+                    (seq {for i in n..((Array.length elements) / n) -> n * i})
+                sieve (position + 1) elements
+
+    sieve 0 [|1..maxOfSieve|] 
+    |> Seq.filter (fun i -> i <> 1)
